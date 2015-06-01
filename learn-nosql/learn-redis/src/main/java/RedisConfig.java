dfe46d309fbc5bb8f3c8eb4deb2adbb6e35ab861
liekkas <zengjiangqin@gmail.com>
2015-06-01 14:04:22 +0800

    add nosql

---
+++ learn-nosql/learn-redis/src/main/java/RedisConfig.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) www.ultrapower.com.cn
+ */
+
+public class RedisConfig {
+
+    public static final int MaxTotal = 500;
+    public static final int MaxIdle = 100;
+    public static final int MaxWaitMillis = 1000*1000;
+    public static final int Timeout = 1000;
+    public static final int RetryNum = 5;
+}
